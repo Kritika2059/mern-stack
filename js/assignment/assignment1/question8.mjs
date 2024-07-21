313c976bdb235cac2397507fbb123b63b51041bb
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-07-21 19:33:22 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question8.mjs
@@ -0,0 +1,11 @@
+//Create an arrow function named isEqualTo10 that takes one 
+//input and returns true if the given number is exactly 10, 
+//otherwise returns false.
+
+const isEqualTo10 = (number) => number === 10;
+
+// Example
+console.log(isEqualTo10(10)); // true
+console.log(isEqualTo10(9)); // false
+console.log(isEqualTo10(15)); // false
+// console.log(isEqualTo10(10.0)); // true, since 10 and 10.0 are considered equal in JavaScript
