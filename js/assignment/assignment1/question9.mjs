f53a602578f7d237fd0392c8ece215211bc07e06
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-07-22 14:07:32 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question9.mjs
@@ -0,0 +1,11 @@
+//Create an arrow function named isLessThan5 that takes one 
+//input and returns true if the given number is less than 5, 
+//otherwise returns false.
+
+const isLessThan5 = (number) => number < 5;
+
+// Example
+console.log(isLessThan5(4)); // true
+console.log(isLessThan5(5)); // false
+console.log(isLessThan5(6)); // false
+// console.log(isLessThan5(4.9)); // true, since 4.9 is less than 5 in JavaScript
