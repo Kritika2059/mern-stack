2c69f8485185ce59d9d1d52e2575f67c777a40de
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-07-24 23:18:20 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question10.mjs
@@ -0,0 +1,10 @@
+// Create an arrow function named isEmptyArray that takes one
+// input and returns true if the given array is empty,
+// otherwise returns false.
+
+const isEmptyArray = (arr) => arr.length === 0;
+
+// Example
+console.log(isEmptyArray([])); // true
+console.log(isEmptyArray([1, 2, 3])); // false
+console.log(isEmptyArray([null])); // false, since an array with null is not empty in JavaScript
