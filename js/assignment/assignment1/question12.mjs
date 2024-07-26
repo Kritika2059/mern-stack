32eed2dca9691b88dbaf4240d4ab861d15fcb7dc
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-07-26 21:03:48 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question12.mjs
@@ -0,0 +1,10 @@
+// Create an arrow function named isEmptyString that takes one
+// input and returns true if the given string is empty,
+// otherwise returns false.
+
+const isEmptyString = (str) => str === "";
+
+// Example
+console.log(isEmptyString("")); // true
+console.log(isEmptyString("Hello")); // false
+console.log(isEmptyString(" ")); // false, since a space is not an empty string in JavaScript
