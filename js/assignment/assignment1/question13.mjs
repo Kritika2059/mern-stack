e79ed792e14b7ea8fd1e99fafdcf46887fa1c66e
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-07-27 21:36:16 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question13.mjs
@@ -0,0 +1,11 @@
+// Create an arrow function named isEven that takes one
+// input and returns true if the given number is even,
+// otherwise returns false.
+
+const isEven = (number) => number % 2 === 0;
+
+// Example
+console.log(isEven(2)); // true
+console.log(isEven(3)); // false
+console.log(isEven(4)); // true
+// console.log(isEven(0)); // true, since 0 is even in JavaScript
