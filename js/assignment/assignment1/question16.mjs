70f09d42f028699628bd33e494c4ceab56c4a6a7
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-08-03 21:42:50 +0545

    Add files via upload

---
+++ js/assignment/assignment1/question16.mjs
@@ -0,0 +1,11 @@
+// Create an arrow function named isPositive that takes one 
+// input and returns true if the given number is positive, 
+// otherwise returns false.
+
+const isPositive = (number) => number > 0;
+
+// Example
+console.log(isPositive(1)); // true
+console.log(isPositive(0)); // false
+console.log(isPositive(-1)); // false
+// console.log(isPositive(0.1)); // true, since 0.1 is greater than 0 in JavaScript
